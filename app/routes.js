25658b4d1b9b970e6adc56dd63e6a12dbdbca402
saiht <mathias_cadet@yahoo.fr>
2016-10-10 08:57:22 +0200

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,4 @@
-/**
- * 
- * AngularJS Boilerplate
- * @description           Description
- * @author                Jozef Butko // www.jozefbutko.com/resume
- * @url                   www.jozefbutko.com
- * @version               1.1.7
- * @date                  March 2015
- * @license               MIT
- * 
- */
+
 ;(function() {
 
 angular.module('chat', ['ngRoute'])
