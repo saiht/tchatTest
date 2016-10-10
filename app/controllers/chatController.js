ff132faac56e82374cd5044692eccf0be01a5b75
saiht <mathias_cadet@yahoo.fr>
2016-10-10 08:59:34 +0200

    ok

--- app/controllers/chatController.js
+++ app/controllers/chatController.js
@@ -1,12 +1,6 @@
 'use strict';
 
-/**
- * Main application controller
- *
- * You can use this controller for your whole app if it is small
- * or you can have separate controllers for each logical section
- * 
- */
+
 ;(function() {
 
   angular
